6f5d448d9a97ff3e1f18d6841b13ae0c14a93a46
Michel Lima <michelpl@yahoo.com.br>
2018-05-10 15:14:23 -0300

    :fire: Remove Serialize.

--- Model/Product/ProductPlan/Plan.php
+++ Model/Product/ProductPlan/Plan.php
@@ -93,7 +93,6 @@ class Plan extends \Magento\Catalog\Model\Product\Type\AbstractType
      * @param \Magento\Catalog\Model\Product\Attribute\Source\Status $catalogProductStatus
      * @param \Magento\Framework\App\State $appState
      * @param \Magento\Msrp\Helper\Data $msrpData
-     * @param \Magento\Framework\Serialize\Serializer\Json|null $serializer
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
@@ -110,8 +109,7 @@ class Plan extends \Magento\Catalog\Model\Product\Type\AbstractType
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Catalog\Model\Product\Attribute\Source\Status $catalogProductStatus,
         \Magento\Framework\App\State $appState,
-        \Magento\Msrp\Helper\Data $msrpData,
-        \Magento\Framework\Serialize\Serializer\Json $serializer = null
+        \Magento\Msrp\Helper\Data $msrpData
     ) {
         $this->productLinks = $catalogProductLink;
         $this->_storeManager = $storeManager;
@@ -127,8 +125,7 @@ class Plan extends \Magento\Catalog\Model\Product\Type\AbstractType
             $filesystem,
             $coreRegistry,
             $logger,
-            $productRepository,
-            $serializer
+            $productRepository
         );
     }
 
@@ -342,78 +339,78 @@ class Plan extends \Magento\Catalog\Model\Product\Type\AbstractType
         return $productsInfo;
     }
 
-    /**
-     * Prepare product and its configuration to be added to some products list.
-     * Perform standard preparation process and add logic specific to Grouped product type.
-     *
-     * @param \Magento\Framework\DataObject $buyRequest
-     * @param \Magento\Catalog\Model\Product $product
-     * @param string $processMode
-     * @return \Magento\Framework\Phrase|array|string
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
-     */
-    protected function _prepareProduct(\Magento\Framework\DataObject $buyRequest, $product, $processMode)
-    {
-        $products = [];
-        $associatedProductsInfo = [];
-        $isStrictProcessMode = $this->_isStrictProcessMode($processMode);
-        $productsInfo = $this->getProductInfo($buyRequest, $product, $isStrictProcessMode);
-        if (is_string($productsInfo)) {
-            return $productsInfo;
-        }
-        $associatedProducts = !$isStrictProcessMode || !empty($productsInfo)
-            ? $this->getAssociatedProducts($product)
-            : false;
-
-        foreach ($associatedProducts as $subProduct) {
-            $qty = $productsInfo[$subProduct->getId()];
-            if (!is_numeric($qty) || empty($qty)) {
-                continue;
-            }
-
-            $_result = $subProduct->getTypeInstance()->_prepareProduct($buyRequest, $subProduct, $processMode);
-
-            if (is_string($_result)) {
-                return $_result;
-            } elseif (!isset($_result[0])) {
-                return __('Cannot process the item.')->render();
-            }
-
-            if ($isStrictProcessMode) {
-                $_result[0]->setCartQty($qty);
-                $_result[0]->addCustomOption('product_type', self::TYPE_CODE, $product);
-                $_result[0]->addCustomOption(
-                    'info_buyRequest',
-                    $this->serializer->serialize(
-                        [
-                            'super_product_config' => [
-                                'product_type' => self::TYPE_CODE,
-                                'product_id' => $product->getId(),
-                            ],
-                        ]
-                    )
-                );
-                $products[] = $_result[0];
-            } else {
-                $associatedProductsInfo[] = [$subProduct->getId() => $qty];
-                $product->addCustomOption('associated_product_' . $subProduct->getId(), $qty);
-            }
-        }
-
-        if (!$isStrictProcessMode || count($associatedProductsInfo)) {
-            $product->addCustomOption('product_type', self::TYPE_CODE, $product);
-            $product->addCustomOption('info_buyRequest', $this->serializer->serialize($buyRequest->getData()));
-
-            $products[] = $product;
-        }
-
-        if (count($products)) {
-            return $products;
-        }
-
-        return __('Please specify the quantity of product(s).')->render();
-    }
+//    /**
+//     * Prepare product and its configuration to be added to some products list.
+//     * Perform standard preparation process and add logic specific to Grouped product type.
+//     *
+//     * @param \Magento\Framework\DataObject $buyRequest
+//     * @param \Magento\Catalog\Model\Product $product
+//     * @param string $processMode
+//     * @return \Magento\Framework\Phrase|array|string
+//     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+//     * @SuppressWarnings(PHPMD.NPathComplexity)
+//     */
+//    protected function _prepareProduct(\Magento\Framework\DataObject $buyRequest, $product, $processMode)
+//    {
+//        $products = [];
+//        $associatedProductsInfo = [];
+//        $isStrictProcessMode = $this->_isStrictProcessMode($processMode);
+//        $productsInfo = $this->getProductInfo($buyRequest, $product, $isStrictProcessMode);
+//        if (is_string($productsInfo)) {
+//            return $productsInfo;
+//        }
+//        $associatedProducts = !$isStrictProcessMode || !empty($productsInfo)
+//            ? $this->getAssociatedProducts($product)
+//            : false;
+//
+//        foreach ($associatedProducts as $subProduct) {
+//            $qty = $productsInfo[$subProduct->getId()];
+//            if (!is_numeric($qty) || empty($qty)) {
+//                continue;
+//            }
+//
+//            $_result = $subProduct->getTypeInstance()->_prepareProduct($buyRequest, $subProduct, $processMode);
+//
+//            if (is_string($_result)) {
+//                return $_result;
+//            } elseif (!isset($_result[0])) {
+//                return __('Cannot process the item.')->render();
+//            }
+//
+//            if ($isStrictProcessMode) {
+//                $_result[0]->setCartQty($qty);
+//                $_result[0]->addCustomOption('product_type', self::TYPE_CODE, $product);
+//                $_result[0]->addCustomOption(
+//                    'info_buyRequest',
+//                    $this->serializer->serialize(
+//                        [
+//                            'super_product_config' => [
+//                                'product_type' => self::TYPE_CODE,
+//                                'product_id' => $product->getId(),
+//                            ],
+//                        ]
+//                    )
+//                );
+//                $products[] = $_result[0];
+//            } else {
+//                $associatedProductsInfo[] = [$subProduct->getId() => $qty];
+//                $product->addCustomOption('associated_product_' . $subProduct->getId(), $qty);
+//            }
+//        }
+//
+//        if (!$isStrictProcessMode || count($associatedProductsInfo)) {
+//            $product->addCustomOption('product_type', self::TYPE_CODE, $product);
+//            $product->addCustomOption('info_buyRequest', $this->serializer->serialize($buyRequest->getData()));
+//
+//            $products[] = $product;
+//        }
+//
+//        if (count($products)) {
+//            return $products;
+//        }
+//
+//        return __('Please specify the quantity of product(s).')->render();
+//    }
 
     /**
      * Retrieve products divided into groups required to purchase
