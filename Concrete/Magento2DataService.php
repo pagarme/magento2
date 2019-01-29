c0499da95b43d3dc238b702a4cc38cf567f0fb2f
Ian Lessa <ian.lessa1@gmail.com>
2019-01-29 18:56:25 -0200

    :bug: fix capture transaction additional info update

--- Concrete/Magento2DataService.php
+++ Concrete/Magento2DataService.php
@@ -5,6 +5,7 @@ namespace MundiPagg\MundiPagg\Concrete;
 use Magento\Framework\App\ObjectManager;
 use Mundipagg\Core\Kernel\Abstractions\AbstractDataService;
 use Mundipagg\Core\Kernel\Aggregates\Order;
+use Mundipagg\Core\Kernel\ValueObjects\Id\ChargeId;
 
 class Magento2DataService extends AbstractDataService
 {
@@ -17,59 +18,115 @@ class Magento2DataService extends AbstractDataService
         $lastTransId = $platformOrder->getPayment()->getLastTransId();
         $paymentId = $platformOrder->getPayment()->getEntityId();
         $orderId = $platformOrder->getPayment()->getParentId();
-        $transaction = $transactionRepository->getByTransactionId(
+
+        $transactionAuth = $transactionRepository->getByTransactionId(
+            str_replace('-capture', '', $lastTransId),
+            $paymentId,
+            $orderId
+        );
+
+        $transactionCapture = $transactionRepository->getByTransactionId(
             $lastTransId,
             $paymentId,
             $orderId
         );
-        if ($transaction !== false) {
 
-            $orderCreationResponse =
-                $transaction->getAdditionalInformation('mundipagg_payment_module_api_response');
+        //to prevent overwriting auth transaction
+        if ($transactionAuth->getTxnId() === $transactionCapture->getTxnId())
+        {
+            return;
+        }
 
-            if ($orderCreationResponse === null) {
-                return;
-            }
-
-            $orderCreationResponse = json_decode($orderCreationResponse);
-
-            $responseCharges = $orderCreationResponse->charges;
+        if ($transactionAuth !== false) {
             $currentCharges = $order->getCharges();
 
             foreach($currentCharges as $charge) {
-                $outdatedCharge = null;
-                foreach ($responseCharges as $responseCharge)
-                {
-                    if ($responseCharge->id === $charge->getMundipaggId()->getValue()) {
-                        $outdatedCharge = $responseCharge;
-                    }
-                }
-                if ($outdatedCharge === null) {
+                $baseKey = $this->getChargeBaseKey($transactionAuth, $charge);
+                if ($baseKey === null) {
                     continue;
                 }
 
-                $additionalInformation = $transaction->getAdditionalInformation();
-                $lastTransaction = $charge->getLastTransaction();
-                foreach ($additionalInformation as $key => $value) {
-                    if (
-                        strpos($key, 'acquirer_nsu') !== false &&
-                        $value === $outdatedCharge->last_transaction->acquirer_nsu
-                    ) {
-                        $baseKey = str_replace('_acquirer_nsu', '', $key);
-                        $transaction->setAdditionalInformation(
-                            $baseKey . '_acquirer_nsu',
-                            $lastTransaction->getAcquirerNsu()
-                        );
-                        //@fixme is it necessary update other info besides nsu?
-                        /*$transaction->setAdditionalInformation(
-                            $baseKey . '_acquirer_tid',
-                            $lastTransaction->getAcquirerTid()
-                        );*/
-                        $transaction->save();
-                        break;
-                    }
-                }
+                $lastMundipaggTransaction = $charge->getLastTransaction();
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_acquirer_nsu',
+                    $lastMundipaggTransaction->getAcquirerNsu()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_acquirer_tid',
+                    $lastMundipaggTransaction->getAcquirerTid()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_acquirer_auth_code',
+                    $lastMundipaggTransaction->getAcquirerAuthCode()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_acquirer_name',
+                    $lastMundipaggTransaction->getAcquirerName()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_acquirer_message',
+                    $lastMundipaggTransaction->getAcquirerMessage()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_brand',
+                    $lastMundipaggTransaction->getBrand()
+                );
+
+                $transactionCapture->setAdditionalInformation(
+                    $baseKey . '_installments',
+                    $lastMundipaggTransaction->getInstallments()
+                );
+            }
+
+            $transactionCapture->save();
+        }
+    }
+
+    private function getChargeBaseKey($transactionAuth, $charge)
+    {
+        $orderCreationResponse =
+            $transactionAuth->getAdditionalInformation('mundipagg_payment_module_api_response');
+
+        if ($orderCreationResponse === null) {
+            return null;
+        }
+
+        $orderCreationResponse = json_decode($orderCreationResponse);
+
+        $authCharges = $orderCreationResponse->charges;
+
+        $outdatedCharge = null;
+        foreach ($authCharges as $authCharge) {
+            if ($charge->getMundipaggId()->equals(new ChargeId($authCharge->id)))
+            {
+                $outdatedCharge = $authCharge;
             }
         }
+
+        if ($outdatedCharge === null) {
+            return null;
+        }
+
+        try {
+            //if it have no nsu, then it isn't a credit_card transaction;
+            $lastNsu = $outdatedCharge->last_transaction->acquirer_nsu;
+        }catch (\Throwable $e) {
+            return null;
+        }
+
+        $additionalInformation = $transactionAuth->getAdditionalInformation();
+        foreach ($additionalInformation as $key => $value) {
+            if ($value == $lastNsu) {
+                return str_replace('_acquirer_nsu', '', $key);
+            }
+        }
+
+        return null;
     }
 }
