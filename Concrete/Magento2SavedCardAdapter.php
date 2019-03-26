1d67501d5308b79ededc290b66df3ef5d5dd49c2
Ian Lessa <ian.lessa1@gmail.com>
2019-03-26 14:14:40 -0300

    :sparkles: listing core saved cards on customer dash

---
+++ Concrete/Magento2SavedCardAdapter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MundiPagg\MundiPagg\Concrete;
+
+use Mundipagg\Core\Payment\Aggregates\SavedCard;
+
+final class Magento2SavedCardAdapter
+{
+    private $adaptee;
+
+    public function __construct(SavedCard $adaptee)
+    {
+        $this->adaptee = $adaptee;
+    }
+
+    public function getBrand()
+    {
+        return $this->adaptee->getBrand()->getName();
+    }
+
+    public function getLastFourNumbers()
+    {
+        return $this->adaptee->getLastFourDigits()->getValue();
+    }
+
+    public function getCreatedAt()
+    {
+        return null;
+    }
+
+    public function getId()
+    {
+        return 'mp_core_' . $this->adaptee->getId();
+    }
+
+    public function getFirstSixDigits()
+    {
+        return $this->adaptee->getFirstSixDigits()->getValue();
+    }
+
+    public function getMaskedNumber()
+    {
+        $firstSix = $this->getFirstSixDigits();
+        $lastFour = $this->getLastFourNumbers();
+
+        $firstSix = number_format($firstSix/100, 2, '.', '');
+
+        return $firstSix . '**.****.****.' . $lastFour;
+    }
+}
