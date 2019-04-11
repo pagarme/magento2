94a1d64e5fce4b3fee5c3ae4da5141a6e6fee35a
Michel Lima <michelpl@yahoo.com.br>
2019-04-11 15:02:45 -0300

    :loud_sound: Add logs for order canceling fail.

--- Observer/OrderCancelAfter.php
+++ Observer/OrderCancelAfter.php
@@ -8,6 +8,8 @@ use Magento\Framework\Event\Observer as EventObserver;
 use Mundipagg\Core\Kernel\Exceptions\AbstractMundipaggCoreException;
 use Mundipagg\Core\Kernel\Factories\OrderFactory;
 use Mundipagg\Core\Kernel\Services\OrderService;
+use Mundipagg\Core\Kernel\Services\OrderLogService;
+use Mundipagg\Core\Kernel\Services\LocalizationService;
 use Magento\Framework\Webapi\Exception as M2WebApiException;
 use Magento\Framework\Phrase;
 use MundiPagg\MundiPagg\Concrete\Magento2CoreSetup;
@@ -36,7 +38,10 @@ class OrderCancelAfter implements ObserverInterface
 
             $transaction = $this->getTransaction($platformOrder);
             if ($transaction !== false) {
-                $this->cancelOrderByTransactionInfo($transaction);
+                $this->cancelOrderByTransactionInfo(
+                    $transaction,
+                    $platformOrder->getIncrementId()
+                );
                 return;
             }
 
@@ -59,16 +64,17 @@ class OrderCancelAfter implements ObserverInterface
         return $mundipaggProvider->getModuleStatus();
     }
 
-    private function cancelOrderByTransactionInfo($transaction)
+    private function cancelOrderByTransactionInfo($transaction, $orderId)
     {
         $orderService = new OrderService();
 
-        $orderCreationResponse =
-            $transaction->getAdditionalInformation(
-                'mundipagg_payment_module_api_response'
-            );
+        $additionalInfo =  $transaction->getAdditionalInformation();
+        if (!empty($additionalInfo['mundipagg_payment_module_api_response'])) {
+            $orderCreationResponse =
+                $additionalInfo['mundipagg_payment_module_api_response'];
+        }
 
-        if ($orderCreationResponse !== null) {
+        if (!empty($orderCreationResponse)) {
             $orderCreationResponse = json_decode(
                 $orderCreationResponse,
                 true
@@ -80,6 +86,21 @@ class OrderCancelAfter implements ObserverInterface
             $orderService->cancelAtMundipagg($order);
             return;
         }
+
+        $i18n = new LocalizationService();
+        $message1 =
+            "Can't cancel current order. Please cancel it by Mundipagg panel";
+        $message2 = "Charge not found";
+
+        $ExceptionMessage =
+            $i18n->getDashboard($message1) . ' | ' .
+            $i18n->getDashboard($message2);
+
+        $e = new \Exception($ExceptionMessage);
+        $log = new OrderLogService();
+        $log->orderException($e, $orderId);
+
+        throw $e;
     }
 
     private function cancelOrderByIncrementId($incrementId)
