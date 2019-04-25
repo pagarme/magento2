2a8c1e986544ce8bd3e3782ff0b27dfa34bbe2b1
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2019-04-25 17:06:41 -0300

    :recycle: Refactoring to move charge process to core

--- Controller/Adminhtml/Charges/Cancel.php
+++ Controller/Adminhtml/Charges/Cancel.php
@@ -2,30 +2,9 @@
 
 namespace MundiPagg\MundiPagg\Controller\Adminhtml\Charges;
 
-use Mundipagg\Core\Kernel\Factories\TransactionFactory;
-use Mundipagg\Core\Kernel\ValueObjects\Id\OrderId;
-
 use Mundipagg\Core\Kernel\Repositories\ChargeRepository;
-use Mundipagg\Core\Kernel\Services\APIService;
+use Mundipagg\Core\Kernel\Services\ChargeService;
 use Mundipagg\Core\Kernel\ValueObjects\Id\ChargeId;
-
-use Mundipagg\Core\Kernel\Aggregates\Charge;
-use Mundipagg\Core\Kernel\Aggregates\Order;
-use Mundipagg\Core\Kernel\Factories\OrderFactory;
-use Mundipagg\Core\Kernel\Interfaces\PlatformOrderInterface;
-use Mundipagg\Core\Kernel\Repositories\OrderRepository;
-use Mundipagg\Core\Kernel\Services\LocalizationService;
-use Mundipagg\Core\Kernel\Services\MoneyService;
-use Mundipagg\Core\Kernel\Services\OrderService;
-use Mundipagg\Core\Webhook\Services\ChargeHandlerService;
-
-use Mundipagg\Core\Kernel\ValueObjects\ChargeStatus;
-use Mundipagg\Core\Kernel\ValueObjects\OrderStatus;
-use Mundipagg\Core\Kernel\ValueObjects\OrderState;
-use Mundipagg\Core\Kernel\ValueObjects\TransactionType;
-use Mundipagg\Core\Kernel\Services\LogService;
-use MundiAPILib\Models\GetChargeResponse;
-use Mundipagg\Core\Payment\Services\ResponseHandlers\OrderHandler;
 
 class Cancel extends ChargeAction
 {
@@ -37,87 +16,26 @@ class Cancel extends ChargeAction
     public function execute()
     {
         parent::execute();
-        $logService = new LogService(
-            'Charge.Cancel',
-            true
-        );
-
-        $orderRepository = new OrderRepository();
-        $chargeRepository = new ChargeRepository();
-        $orderService = new OrderService();
-        $moneyService = new MoneyService();
-        $chargeHandlerService = new ChargeHandlerService();
-        $i18n = new LocalizationService();
-
         $params = $this->request->getParams();
 
         if (!isset($params['amount']) || !isset($params['chargeId'])) {
             $logService->info("Amount or Chage not found");
-            return $this->handlerFail("Amount or ChardId not found");
+            return $this->responseFail("Amount or Charge not found");
         }
 
         $amount = str_replace([',', '.'], "", $params['amount']);
         $chargeId = $params['chargeId'];
-        $orderId = $params['orderId'];
 
-        $order = $orderRepository->findByMundipaggId(
-            new OrderId($orderId)
-        );
-
-        $platformOrder = $order->getPlatformOrder();
-
+        $chargeRepository = new ChargeRepository();
         $charge = $chargeRepository->findByMundipaggId(
             new ChargeId($chargeId)
         );
 
-        $apiService = new APIService();
-        $logService->info("Cancel charge on Mundipagg - " . $chargeId);
-        $resultApi = $apiService->cancelCharge($charge, $amount);
-
-        if ($resultApi === null) {
-
-            $order->updateCharge($charge);
-
-            $orderRepository->save($order);
-            $history = $chargeHandlerService->prepareHistoryComment($charge);
-
-            $order->getPlatformOrder()->addHistoryComment($history);
-            $orderService->syncPlatformWith($order);
-
-            $platformOrderGrandTotal = $moneyService->floatToCents(
-                $platformOrder->getGrandTotal()
-            );
-            $platformOrderTotalCanceled = $moneyService->floatToCents(
-                $platformOrder->getTotalCanceled()
-            );
-
-            $platformOrderTotalRefunded = $moneyService->floatToCents(
-                $platformOrder->getTotalRefunded()
-            );
-
-            if (
-                $platformOrderGrandTotal === $platformOrderTotalCanceled ||
-                $platformOrderGrandTotal === $platformOrderTotalRefunded
-            ) {
-                $logService->info("Change Order status");
-
-                $order->setStatus(OrderStatus::canceled());
-                $order->getPlatformOrder()->setState(OrderState::canceled());
-                $order->getPlatformOrder()->save();
-
-                $order->getPlatformOrder()->addHistoryComment(
-                    $i18n->getDashboard('Order canceled.')
-                );
-
-                $orderRepository->save($order);
-
-                $orderService->syncPlatformWith($order);
-            }
-
-            $message = "Charge canceled with success";
-            return $this->responseSuccess($message);
+        $chargeService = new ChargeService($charge);
+        $response = $chargeService->cancel($amount);
+        if ($response->isSuccess()) {
+            return $this->responseSuccess($response->getMessage());
         }
-
-        return $this->responseFail($resultApi);
+        return $this->responseFail($response->getMessage());
     }
 }
--- Controller/Adminhtml/Charges/Capture.php
+++ Controller/Adminhtml/Charges/Capture.php
@@ -2,29 +2,9 @@
 
 namespace MundiPagg\MundiPagg\Controller\Adminhtml\Charges;
 
-use Mundipagg\Core\Kernel\Factories\TransactionFactory;
-use Mundipagg\Core\Kernel\ValueObjects\Id\OrderId;
-
 use Mundipagg\Core\Kernel\Repositories\ChargeRepository;
-use Mundipagg\Core\Kernel\Services\APIService;
+use Mundipagg\Core\Kernel\Services\ChargeService;
 use Mundipagg\Core\Kernel\ValueObjects\Id\ChargeId;
-
-use Mundipagg\Core\Kernel\Aggregates\Charge;
-use Mundipagg\Core\Kernel\Aggregates\Order;
-use Mundipagg\Core\Kernel\Factories\OrderFactory;
-use Mundipagg\Core\Kernel\Interfaces\PlatformOrderInterface;
-use Mundipagg\Core\Kernel\Repositories\OrderRepository;
-use Mundipagg\Core\Kernel\Services\LocalizationService;
-use Mundipagg\Core\Kernel\Services\MoneyService;
-use Mundipagg\Core\Kernel\Services\OrderService;
-use Mundipagg\Core\Webhook\Services\ChargeHandlerService;
-
-use Mundipagg\Core\Kernel\ValueObjects\ChargeStatus;
-use Mundipagg\Core\Kernel\ValueObjects\OrderStatus;
-use Mundipagg\Core\Kernel\ValueObjects\TransactionType;
-use Mundipagg\Core\Kernel\Services\LogService;
-use MundiAPILib\Models\GetChargeResponse;
-use Mundipagg\Core\Payment\Services\ResponseHandlers\OrderHandler;
 
 class Capture extends ChargeAction
 {
@@ -36,18 +16,6 @@ class Capture extends ChargeAction
     public function execute()
     {
         parent::execute();
-        $logService = new LogService(
-            'Charge.Capture',
-            true
-        );
-
-        $orderRepository = new OrderRepository();
-        $chargeRepository = new ChargeRepository();
-        $orderService = new OrderService();
-        $moneyService = new MoneyService();
-        $chargeHandlerService = new ChargeHandlerService();
-        $i18n = new LocalizationService();
-
         $params = $this->request->getParams();
 
         if (!isset($params['amount']) || !isset($params['chargeId'])) {
@@ -57,56 +25,17 @@ class Capture extends ChargeAction
 
         $amount = str_replace([',', '.'], "", $params['amount']);
         $chargeId = $params['chargeId'];
-        $orderId = $params['orderId'];
 
-        $order = $orderRepository->findByMundipaggId(
-            new OrderId($orderId)
-        );
-
-        $platformOrder = $order->getPlatformOrder();
-
+        $chargeRepository = new ChargeRepository();
         $charge = $chargeRepository->findByMundipaggId(
             new ChargeId($chargeId)
         );
 
-        $apiService = new APIService();
-        $logService->info("Capturing charge on Mundipagg - " . $chargeId);
-        $resultApi = $apiService->captureCharge($charge, $amount);
-
-        if ($resultApi instanceof GetChargeResponse) {
-
-            if (!$charge->getStatus()->equals(ChargeStatus::paid())) {
-                $logService->info("Pay charge - " . $chargeId);
-                $charge->pay($amount);
-            }
-
-            if ($charge->getPaidAmount() == 0) {
-                $charge->setPaidAmount($amount);
-            }
-
-            $logService->info("Update Charge on Order");
-            $order->updateCharge($charge);
-            $orderRepository->save($order);
-
-            $logService->info("Adding history on Order");
-            $history = $chargeHandlerService->prepareHistoryComment($charge);
-            $platformOrder->addHistoryComment($history);
-
-
-            $logService->info("Synchronizing with platform Order");
-            $orderService->syncPlatformWith($order);
-
-            $logService->info("Change Order status");
-            $order->setStatus(OrderStatus::paid());
-            $orderHandlerService = new OrderHandler();
-            $cantCreateReason = $orderHandlerService->handle($order);
-
-            $message = $chargeHandlerService->prepareReturnMessage($charge);
-
-            return $this->responseSuccess($message);
+        $chargeService = new ChargeService($charge);
+        $response = $chargeService->capture($amount);
+        if ($response->isSuccess()) {
+            return $this->responseSuccess($response->getMessage());
         }
-
-        $logService->info($resultApi);
-        return $this->responseFail($resultApi);
+        return $this->responseFail($response->getMessage());
     }
 }
