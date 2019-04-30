d231d639f0a3342329dbaafd5a2d457b02ee43e2
Ian Lessa <ian.lessa1@gmail.com>
2019-04-30 16:21:33 -0300

    :bug: fix card deletion flux and change return messages

--- Controller/Customer/Remove.php
+++ Controller/Customer/Remove.php
@@ -8,9 +8,11 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Request\Http;
+use Mundipagg\Core\Kernel\Services\LocalizationService;
 use Mundipagg\Core\Payment\Repositories\CustomerRepository;
 use Mundipagg\Core\Payment\Repositories\SavedCardRepository;
 use MundiPagg\MundiPagg\Concrete\Magento2CoreSetup;
+use MundiPagg\MundiPagg\Concrete\Magento2SavedCardAdapter;
 use MundiPagg\MundiPagg\Model\CardsRepository;
 use MundiPagg\MundiPagg\Gateway\Transaction\Base\Config\Config;
 use MundiPagg\MundiPagg\Helper\Logger;
@@ -65,38 +67,58 @@ class Remove extends Action
         }
 
         $idCard = $this->request->getParam('id');
+        Magento2CoreSetup::bootstrap();
+        $i18n = new LocalizationService();
+        $mask = '';
 
         try {
             $card = $this->cardsRepository->getById($idCard);
+            $mask = '****.****.****.' . $card->getLastFourNumbers();
 
             $matchIds = [];
             preg_match('/mp_core_\d*/', $idCard, $matchIds);
+            $deleteService = $this->cardsRepository;
+            $deleteMethod = 'deleteById';
 
-            if (!isset($matchIds[0])) {
-                $result = $this->cardsRepository->deleteById($idCard);
-            }
-            else {
-                $this->deleteCoreCard(
-                    $idCard,
-                    new NoSuchEntityException(
-                        __('Cards with id "%1" does not exist.', $idCard)
-                    )
-                );
+            if (isset($matchIds[0])) {
+                $mpCardId = filter_var($idCard, FILTER_SANITIZE_NUMBER_INT);
+                $savedCardRepository = new SavedCardRepository();
+                $savedCard = $savedCardRepository->find($mpCardId);
+
+                $cardAdapter = new Magento2SavedCardAdapter($savedCard);
+                $mask = $cardAdapter->getMaskedNumber();
+
+                $deleteService = $this;
+                $deleteMethod = 'deleteCoreCard';
             }
 
             $response =
-            $this
-                ->getApi()
-                ->getCustomers()
-                ->deleteCard(
-                    $card->getCardId(),
-                    $card->getCardToken()
-                );
+                $this
+                    ->getApi()
+                    ->getCustomers()
+                    ->deleteCard(
+                        $card->getCardId(),
+                        $card->getCardToken()
+                    );
             $this->logger->logger($response);
 
-            $this->messageManager->addSuccess(__('You deleted card id: %1', $idCard));
+            $deleteService->$deleteMethod(
+                $idCard,
+                new NoSuchEntityException(
+                    __('Cards with id "%1" does not exist.', $idCard)
+                )
+            );
+
+            $message = $i18n->getDashboard("The card '%s' was deleted.", $mask);
+            $this->messageManager->addSuccess($message);
         } catch (\Exception $e) {
-            $this->messageManager->addError(__($e->getMessage()));
+            //$this->messageManager->addError(__($e->getMessage()));
+            $error = $i18n->getDashboard("The informed card couldn't be deleted.");
+            if (!empty($mask)) {
+                $error = $i18n->getDashboard("The card '%s' couldn't be deleted.", $mask);
+            }
+            $this->messageManager->addError($error);
+
         }
 
         $this->_redirect('mundipagg/customer/cards'); 
@@ -133,6 +155,7 @@ class Remove extends Action
         }
 
         if (!$customer->getMundipaggId()->equals($savedCard->getOwnerId())) {
+            $baseException->setMessage("The logged user doesn't own the informed card.");
             throw $baseException;
         }
 
