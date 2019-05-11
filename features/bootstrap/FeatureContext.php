14dbc92380face786fedd0fec5073dde28e188dd
Michel <michelpl@yahoo.com.br>
2019-05-10 21:48:10 -0300

    :green_heart: Add tests do CI.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,9 +2,7 @@
 
 
 use Mundipagg\Core\Test\Functional\Features\Bootstrap\CoreFeature;
-use Behat\Behat\Tester\Exception\PendingException;
-use Behat\Mink\Exception\ResponseTextException;
-use Behat\MinkExtension\Context\MinkContext;
+
 
 /**
  * Features context.
