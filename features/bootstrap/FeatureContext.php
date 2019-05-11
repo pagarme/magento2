519be643ab6f44d5e7fcf415a6d4042b56099089
Michel <michelpl@yahoo.com.br>
2019-05-10 23:14:59 -0300

    :green_heart: Add tests do CI.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -9,61 +9,6 @@ use Mundipagg\Core\Test\Functional\Features\Bootstrap\CoreFeature;
  */
 class FeatureContext extends CoreFeature
 {
-    /**
-     *
-     * @AfterStep
-     * @param     $event
-     */
-    protected function afterStepFailureScreenshot($event)
-    {
-        $e = $event->getTestResult()->getCallResult()->getException();
-        if ($e) {
-            if (!file_exists($this->screenshotDir)) {
-                mkdir($this->screenshotDir);
-            }
-            $filename = tempnam($this->screenshotDir, "failure_screenshoot_");
-            unlink($filename);
-            $filename .= ".png";
-            $this->screenshot($filename);
-            echo "saved failure screenshot to '$filename'";
-        }
-    }
-
-
-    /**
-     *
-     * @BeforeFeature
-     */
-    protected static function beforeFeature($event)
-    {
-        self::$featureHash = null;
-        $requestTime = $_SERVER['REQUEST_TIME'];
-        $featureTitle = $event->getFeature()->getTitle();
-        $hash = hash('sha512', $featureTitle . $requestTime);
-        self::$featureHash = substr($hash, 0, 16);
-    }
-
-
-    /**
-     *
-     * @BeforeScenario
-     */
-    protected function beforeScenario($event)
-    {
-        if ($event->getScenario()->hasTag('smartStep')) {
-            /*throw new PendingException(
-                'This is a partial @smartStep Scenario and should not be isolatedly executed.'
-            );*/
-        }
-
-        $this->scenarioTokens = null;
-        try {
-            //trying to save examples to use in @smartStep
-            $this->scenarioTokens =
-                $event->getScenario()->getTokens();
-        }catch(Throwable $e) {
-        }
-    }
 
     /**
      *
