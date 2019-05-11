e373bf27b75fcfff7561dabe9fdb8f2e2f590c90
Michel <michelpl@yahoo.com.br>
2019-05-11 00:38:52 -0300

    :green_heart: Add tests do CI.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -10,36 +10,4 @@ use Mundipagg\Core\Test\Functional\Features\Bootstrap\CoreFeature;
 class FeatureContext extends CoreFeature
 {
 
-
-    /**
-     *
-     * @Given /^a new session$/
-     */
-    public function newSession()
-    {
-        $this->getSession()->reset();
-        //throw new Exception("as");
-    }
-
-    /**
-     *
-     * @Given /^I define failure screenshot dir as "([^"]*)"$/
-     */
-    public function setScreenshotDir($dir)
-    {
-        $this->screenshotDir = $dir;
-    }
-
-    /**
-     *
-     * @Given /^I save a screenshot to "([^"]*)" file$/
-     */
-    public function screenshot($filename)
-    {
-        $driver =  $this->getSession()->getDriver();
-        $data = $driver->getScreenshot();
-        $file = fopen($filename, "w");
-        fwrite($file, $data);
-        fclose($file);
-    }
 }
