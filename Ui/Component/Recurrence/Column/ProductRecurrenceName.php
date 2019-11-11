d0ac396fdd17994f51fdc17957c5b7962ac7f062
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2019-11-11 11:51:35 -0300

    :construction: Improve product subscription grid

---
+++ Ui/Component/Recurrence/Column/ProductRecurrenceName.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MundiPagg\MundiPagg\Ui\Component\Recurrence\Column;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class ProductRecurrenceName extends Column
+{
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+
+        $fieldName = $this->getData('name');
+
+        foreach ($dataSource['data']['items'] as &$item) {
+            $item[$fieldName] = $this->getValueFormatted($item);
+        }
+
+        return $dataSource;
+    }
+
+    public function getValueFormatted($item)
+    {
+        $objectManager = ObjectManager::getInstance();
+
+        $product = $objectManager->get('Magento\Catalog\Model\Product')->load($item['product_id']);
+        return $product->getName();
+    }
+}
---
+++ Ui/Component/Recurrence/Column/RepetitionsColumn.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MundiPagg\MundiPagg\Ui\Component\Recurrence\Column;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Mundipagg\Core\Recurrence\Aggregates\Repetition;
+use Mundipagg\Core\Recurrence\Services\ProductSubscriptionService;
+use Mundipagg\Core\Recurrence\ValueObjects\DiscountValueObject;
+use MundiPagg\MundiPagg\Concrete\Magento2CoreSetup;
+
+class RepetitionsColumn extends Column
+{
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        Magento2CoreSetup::bootstrap();
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        foreach ($dataSource['data']['items'] as &$item) {
+            $item['repetitions'] = $this->getValueFormatted($item);
+        }
+
+        return $dataSource;
+    }
+
+    public function getValueFormatted($item)
+    {
+        $id = $item['id'];
+        $productSubscriptionService = new ProductSubscriptionService();
+        $productSubscription = $productSubscriptionService->findById($id);
+        $repetitions = [];
+        foreach ($productSubscription->getRepetitions() as $repetition) {
+            $value = $repetition->getIntervalCount() . " " . $repetition->getIntervalType() . " - ";
+            $value .= "discount: " . $this->getDiscountFormatted($repetition);
+            $repetitions[] = $value;
+        }
+
+        return implode(', ', $repetitions);
+    }
+
+    /** Copy Paste, should change to a presentation classe maybe */
+    protected function getDiscountFormatted(Repetition $repetition)
+    {
+        $discountValue = $repetition->getDiscountValue();
+        $discountType = $repetition->getDiscountType();
+        $symbols = $repetition->getDiscountTypeSymbols();
+        $flat = DiscountValueObject::DISCOUNT_TYPE_FLAT;
+
+        if ($repetition->getDiscount()->getDiscountType() == $flat) {
+            return implode(" ", [
+                $symbols[$discountType],
+                $discountValue
+            ]);
+        }
+
+        return implode("", [
+            $discountValue,
+            $symbols[$discountType]
+        ]);
+    }
+}
