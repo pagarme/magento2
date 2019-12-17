82ae0e14b32f9fbac07cffaee9243a85d03f7fbc
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2019-12-17 18:31:12 -0300

    :sparkles: Add delete plan action

---
+++ Controller/Adminhtml/Plans/Delete.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MundiPagg\MundiPagg\Controller\Adminhtml\Plans;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Message\Factory;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+use Mundipagg\Core\Recurrence\Services\PlanService;
+use Mundipagg\Core\Recurrence\Services\ProductSubscriptionService;
+use MundiPagg\MundiPagg\Concrete\Magento2CoreSetup;
+use MundiPagg\MundiPagg\Model\ProductsSubscriptionFactory;
+
+class Delete extends Action
+{
+    protected $resultPageFactory;
+
+    /**
+     * @var Registry
+     */
+    protected $coreRegistry;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @throws \Exception
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        Registry $coreRegistry,
+        Factory $messageFactory
+    )
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->coreRegistry = $coreRegistry;
+        $this->messageFactory = $messageFactory;
+        Magento2CoreSetup::bootstrap();
+
+        parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $productId = (int)$this->getRequest()->getParam('id');
+        if ($productId) {
+
+            $planService = new PlanService();
+            $productData = $planService->findById($productId);
+
+            if (!$productData || !$productData->getId()) {
+                $message = $this->messageFactory->create('error', __('Plan not exist.'));
+                $this->messageManager->addErrorMessage($message);
+                $this->_redirect('mundipagg_mundipagg/plans/index');
+                return;
+            }
+        }
+
+        $planService->delete($productId);
+
+        $message = $this->messageFactory->create('success', _("Plan deleted."));
+        $this->messageManager->addMessage($message);
+
+        $this->_redirect('mundipagg_mundipagg/plans/index');
+        return;
+    }
+}
