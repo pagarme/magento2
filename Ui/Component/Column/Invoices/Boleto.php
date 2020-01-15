797d595d06a7af16a7cd494d176d1d3d5cf39c02
Michel Lima <michelpl@yahoo.com.br>
2020-01-15 17:32:19 -0300

    :lipstick: Add boleto column to invoice listing.

---
+++ Ui/Component/Column/Invoices/Boleto.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MundiPagg\MundiPagg\Ui\Component\Column\Invoices;
+
+use Magento\Cms\Block\Adminhtml\Page\Grid\Renderer\Action\UrlBuilder;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\UrlInterface;
+
+class Boleto extends Column
+{
+    /** Url path */
+    const URL_PATH_EDIT = 'mundipagg_mundipagg/invoices/index';
+    const URL_PATH_DELETE = 'mundipagg_mundipagg/invoices/delete';
+    /** @var UrlBuilder */
+    protected $actionUrlBuilder;
+    /** @var UrlInterface */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlBuilder $actionUrlBuilder
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlBuilder $actionUrlBuilder,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->actionUrlBuilder = $actionUrlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if ($item['tran_boleto_url'] == '') {
+                    $item['tran_boleto_url'] = ' --- ';
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+}
