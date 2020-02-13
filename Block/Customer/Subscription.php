2135e977ee958c65f476d4ceb9f13dd75adff563
Michel Lima <michelpl@yahoo.com.br>
2020-02-13 19:51:37 -0300

    :sparkles: Show the highest cycle value from a subscription on customer page.

--- Block/Customer/Subscription.php
+++ Block/Customer/Subscription.php
@@ -3,12 +3,15 @@
 namespace MundiPagg\MundiPagg\Block\Customer;
 
 use Magento\Customer\Model\Session;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Mundipagg\Core\Kernel\Abstractions\AbstractEntity;
 use Mundipagg\Core\Kernel\Exceptions\InvalidParamException;
 use Mundipagg\Core\Recurrence\Repositories\SubscriptionRepository;
 use MundiPagg\MundiPagg\Concrete\Magento2CoreSetup;
+use MundiPagg\MundiPagg\Helper\RecurrenceProductHelper;
+use MundiPagg\MundiPagg\Ui\Component\Recurrence\Column\TotalCyclesByProduct;
 
 class Subscription extends Template
 {
@@ -21,6 +24,8 @@ class Subscription extends Template
      * @var SubscriptionRepository
      */
     protected $subscriptionRepository;
+
+    protected $objectManager;
 
     /**
      * Link constructor.
@@ -36,6 +41,7 @@ class Subscription extends Template
 
         $this->customerSession = $customerSession;
         $this->subscriptionRepository = new SubscriptionRepository();
+        $this->objectManager = ObjectManager::getInstance();
     }
 
     /**
@@ -47,5 +53,25 @@ class Subscription extends Template
         return $this->subscriptionRepository->findByCustomerId(
             $this->customerSession->getId()
         );
+    }
+
+    public function getHighestProductCycle($item)
+    {
+        $recurrenceProductHelper = new RecurrenceProductHelper();
+        $magentoOrder =
+            $this->objectManager
+                ->get('Magento\Sales\Model\Order')
+                ->loadByIncrementId($item->getCode());
+        $products = $magentoOrder->getAllItems();
+
+        $cycles = [];
+
+        foreach ($products as $product) {
+            $cycles[] =
+                $recurrenceProductHelper
+                    ->getSelectedRepetitionByProduct($product);
+        }
+
+        return $recurrenceProductHelper->returnHighestCycle($cycles);
     }
 }
