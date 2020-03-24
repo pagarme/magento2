38a448a37a108e9a927c7956a157c59b9f0786b9
Michel Lima <michelpl@yahoo.com.br>
2020-03-24 19:10:53 -0300

    :bug: Remove cupon handle.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,9 +1,7 @@
 var config = {
     map: {
         '*': {
-            jquerymask: 'MundiPagg_MundiPagg/js/plugins/jquery.mask.min',
-            "Magento_SalesRule/js/action/set-coupon-code": 'MundiPagg_MundiPagg/js/action/set-coupon-code',
-            "Magento_SalesRule/js/action/cancel-coupon": 'MundiPagg_MundiPagg/js/action/cancel-coupon'
+            jquerymask: 'MundiPagg_MundiPagg/js/plugins/jquery.mask.min'
         }
     },
     config: {
