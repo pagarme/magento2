0ae2ba9a1d1642974ee4012ffd31b18bbc990990
Michel Lima <michelpl@yahoo.com.br>
2020-04-27 15:03:00 -0300

    :construction: Creating basic forms.

---
+++ view/adminhtml/web/js/payment.js
@@ -0,0 +1,11 @@
+require([
+    "jquery",
+    "jquery/ui",
+    "MundiPagg_MundiPagg/js/teste"
+], function ($) {
+    'use strict';
+
+    $(document).ready(function(){
+        console.log("carregou o payment")
+    });
+});
