63719389cf8244edd9ae40de1befd1c3e2c1205b
Gabriel Gonçalves <gabrielll_07@hotmail.com>
2020-05-28 16:34:27 -0300

    :ok_hand: Code Review

--- Block/Adminhtml/Form/Field/CcTypes.php
+++ Block/Adminhtml/Form/Field/CcTypes.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace Mundipagg\Mundipagg\Block\Adminhtml\Form\Field;
 
 use Magento\Braintree\Helper\CcType;
