5a918f974d4adcc878f289974a1ebdb21992eae4
Yan Pantoja <yanmpantoja@gmail.com>
2021-05-07 18:21:51 -0300

    :art: changes jquery method

--- view/adminhtml/web/js/integrationType.js
+++ view/adminhtml/web/js/integrationType.js
@@ -4,12 +4,10 @@ require([
 ], function ($) {
     $(document).ready(function(){
         var integrationTypeElement = $("select[id*='pagarme_pagarme_global_is_gateway_integration_type']");
-        console.log(integrationTypeElement);
         changeCommentsByIntegrionType(integrationTypeElement.value);
 
-        integrationTypeElement.change(function () {
+        integrationTypeElement.on("change", function () {
             changeCommentsByIntegrionType(this.value);
-            console.log(integrationTypeElement.value);
         });
 
         function changeCommentsByIntegrionType(integrationType) {
