108f07a19b2114a34f297e8b2fa67023600c1fe3
netorodrigues <jose.oneto@mundipagg.com>
2021-05-17 10:29:27 -0300

    ✨ stores and uses hub id

--- Block/Adminhtml/Form/Field/HubIntegration.php
+++ Block/Adminhtml/Form/Field/HubIntegration.php
@@ -4,6 +4,8 @@ namespace Pagarme\Pagarme\Block\Adminhtml\Form\Field;
 
 use Magento\Framework\Data\Form\Element\AbstractElement;
 use Magento\Config\Block\System\Config\Form\Field;
+use Pagarme\Core\Hub\Services\HubIntegrationService;
+use Pagarme\Pagarme\Concrete\Magento2CoreSetup;
 
 class HubIntegration extends Field
 {
@@ -14,12 +16,57 @@ class HubIntegration extends Field
      */
     protected function _renderValue(AbstractElement $element)
     {
-
+        Magento2CoreSetup::bootstrap();
         $html = '<td class="value">';
         $html .= $this->_getElementHtml($element);
-        $html .= '<p id="botao-hub"></p>';
+        $html .= sprintf('
+        <p
+        id="botao-hub"
+        hub-url="%s"
+        button-text="%s"></p>', $this->getHubUrl(), __($this->getButtonText()));
         $html .= '</td>';
 
         return $html;
+    }
+
+    private function getButtonText()
+    {
+        $installId = Magento2CoreSetup::getModuleConfiguration()->getHubInstallId();
+        return $installId ? "View Integration" : "Integrate With Mundipagg";
+    }
+
+    private function getHubUrl()
+    {
+        $installId = Magento2CoreSetup::getModuleConfiguration()->getHubInstallId();
+        return $installId ? $this->getBaseViewUrl($installId->getValue()) : $this->getBaseIntegrateUrl();
+    }
+
+    private function getBaseIntegrateUrl()
+    {
+        return "https://stghub.mundipagg.com/apps/{$this->getPublicAppKey()}/authorize?redirect={$this->getRedirectUrl()}&install_token/{$this->getInstallToken()}";
+    }
+
+    private function getBaseViewUrl($installId)
+    {
+        return "https://stghub.mundipagg.com/apps/{$this->getPublicAppKey()}/edit/{$installId}";
+    }
+
+    private function getPublicAppKey()
+    {
+        return Magento2CoreSetup::getHubAppPublicAppKey();
+    }
+
+    private function getRedirectUrl()
+    {
+        return $this->getUrl('pagarme_pagarme/hub/index');
+    }
+
+    private function getInstallToken()
+    {
+        $installSeed = uniqid();
+        $hubIntegrationService = new HubIntegrationService();
+        $installToken = $hubIntegrationService->startHubIntegration($installSeed);
+
+        return $installToken->getValue();
     }
 }
