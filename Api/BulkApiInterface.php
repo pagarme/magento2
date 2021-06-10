23321ff83b12513722a048f228fde997081e7578
Yan Pantoja <yanmpantoja@gmail.com>
2021-06-10 09:00:05 -0300

    :sparkles: adds new route in the api to cancel multiple charges

---
+++ Api/BulkApiInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pagarme\Pagarme\Api;
+
+interface BulkApiInterface
+{
+    /**
+     * @return mixed
+     */
+    public function execute();
+}
---
+++ Model/Api/Bulk.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Pagarme\Pagarme\Model\Api;
+
+use Magento\Framework\HTTP\Client\Curl;
+use Magento\Framework\Webapi\Exception as MagentoException;
+use Magento\Framework\Webapi\Rest\Request;
+use Magento\Setup\Exception;
+use Magento\Store\Model\StoreManagerInterface;
+use Pagarme\Pagarme\Api\BulkApiInterface;
+use Pagarme\Pagarme\Concrete\Magento2CoreSetup;
+
+class Bulk implements BulkApiInterface
+{
+    /**
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * @var \Magento\Framework\HTTP\Client\Curl
+     */
+    protected $curl;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private StoreManagerInterface $storeManager;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @var array
+     */
+    private $responseArray;
+
+    public function __construct(
+        Request $request,
+        \Magento\Framework\HTTP\Client\Curl $curl,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->request = $request;
+        $this->curl = $curl;
+        $this->baseUrl = $storeManager->getStore()->getBaseUrl();
+    }
+
+    /**
+     * @return array|mixed
+     */
+    public function execute()
+    {
+        $requestParams = $this->request->getBodyParams();
+        $accessToken = $requestParams['access_token'];
+        $this->curl->addHeader("Authorization", "Bearer {$accessToken}");
+
+        foreach ($requestParams['requests'] as $key => $request) {
+            $this->executeCurl($key, $request);
+        }
+
+        return $this->getFormatedResponse();
+    }
+
+    private function executeCurl(int $key, array $request): void
+    {
+        $method = $request['method'];
+        $apiUrl = $this->getChargeUrl() . $request['path'];
+        $params = $request['params'];
+
+        try {
+            $this->curl->$method($apiUrl, $params);
+            $curlResponseRequest = $this->curl;
+        }catch (\Exception $exception) {
+            throw new MagentoException(__($exception->getMessage()), 0, 500);
+        }
+
+        $this->setFormatedResponse(
+            $key,
+            $curlResponseRequest,
+            $request
+        );
+    }
+
+    private function getChargeUrl(): string
+    {
+        $defaultStoreViewCode = Magento2CoreSetup::getDefaultStoreViewCode();
+        return $this->baseUrl . "rest/{$defaultStoreViewCode}/V1/pagarme";
+    }
+
+    private function setFormatedResponse(
+        int $index,
+        Curl $response,
+        array $request
+    ): void {
+        $this->responseArray[] = array(
+            "index" => $index,
+            "status" => $response->getStatus(),
+            "body" => json_decode($response->getBody(), true),
+            "path" => $request['path'],
+            "method" => $request['method'],
+        );
+    }
+
+    private function getFormatedResponse(): array
+    {
+        return $this->responseArray;
+    }
+}
