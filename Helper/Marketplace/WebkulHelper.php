67deab12dfe2514adf1deed9acaefb04df3d257f
Yan Pantoja <yanmpantoja@gmail.com>
2021-09-09 15:39:23 -0300

    :sparkles: adds webkul integration

--- Helper/Marketplace/WebkulHelper.php
+++ Helper/Marketplace/WebkulHelper.php
@@ -13,39 +13,34 @@
 namespace Pagarme\Pagarme\Helper\Marketplace;
 
 use Magento\Framework\App\ObjectManager;
-
-
+use Pagarme\Core\Kernel\Services\MoneyService;
+use Pagarme\Core\Marketplace\Services\RecipientService;
+use Webkul\Marketplace\Helper\Payment;
+use Webkul\Marketplace\Helper\Data;
 
 class WebkulHelper
 {
-    const WEBKUL_PRODUCT_COLLECTION_FACTORY_PATH =
-    'Webkul\\Marketplace\\Model\\ResourceModel\\Product\\CollectionFactory ';
-    const WEBKUL_SALEPERPARTNER_COLLECTION_FACTORY_PATH =
-    'Webkul\\Marketplace\\Model\\ResourceModel\\Saleperpartner\\CollectionFactory';
-
     private $productCollectionFactory;
     private $salesPerPartnerCollectionFactory;
     private $objectManager;
+    private $recipientService;
 
     private $enabled = false;
 
-    public function __construct()
-    {
+    public function __construct() {
+
         $this->objectManager = ObjectManager::getInstance();
-        if (
-            !class_exists(self::WEBKUL_PRODUCT_COLLECTION_FACTORY_PATH) ||
-            !class_exists(self::WEBKUL_SALEPERPARTNER_COLLECTION_FACTORY_PATH)
-        ) {
-            return;
+
+        if (!class_exists('Webkul\\Marketplace\\Helper\\Payment')) {
+            return null;
         }
 
-        $this->productCollectionFactory = $this->objectManager->get(
-            self::WEBKUL_PRODUCT_COLLECTION_FACTORY_PATH
-        );
-        $this->salesPerPartnerCollectionFactory = $this->objectManager->get(
-            self::WEBKUL_SALEPERPARTNER_COLLECTION_FACTORY_PATH
-        );
+        $this->mpPaymentHelper = $this->objectManager->get(Payment::class);
+
         $this->setEnabled(true);
+
+        $this->recipientService = new RecipientService();
+        $this->moneyService = new MoneyService();
     }
 
     public function isEnabled()
@@ -60,7 +55,112 @@ class WebkulHelper
 
     public function getSplitDataFromOrder($platformOrder)
     {
-        var_dump($platformOrder);
-        return true;
+        $orderItems = $platformOrder->getAllItems();
+        $splitData['sellers'] = [];
+        $totalPaid = (int) $this->moneyService->floatToCents(
+            $platformOrder->getGrandTotal()
+        );
+
+        foreach ($orderItems as $item) {
+
+            $productId = $item->getProductId();
+            $sellerDetail = $this->mpPaymentHelper->getSellerDetail($productId);
+            $sellerId = $sellerDetail['id'];
+            $itemPrice = (int) $this->moneyService->floatToCents(
+                $item->getRowTotal()
+            );
+
+            if(!$sellerId) {
+                continue;
+            }
+
+            $splitData = $this->sumSellersCommissionsIfArrayKeyExists(
+                $itemPrice,
+                $splitData,
+                $sellerDetail
+            );
+
+            $splitData = $this->setNewKeyIfArrayKeyNotExists(
+                $itemPrice,
+                $splitData,
+                $sellerDetail
+            );
+        }
+
+        if(!$sellerId && empty($splitData['sellers'])) {
+            return null;
+        }
+
+        $marketplaceCommission
+            = $totalPaid - $this->getTotalSellerCommission(
+                $splitData['sellers']
+            );
+        $splitData['marketplace']
+            = ['marketplaceCommission' => $marketplaceCommission];
+
+        return $splitData;
+    }
+
+    private function getSellerData($itemPrice, $sellerDetail)
+    {
+        $percentageCommission = $sellerDetail['commission'] / 100;
+        $sellerCommission = $itemPrice * $percentageCommission;
+
+        $recipient = $this->recipientService->findRecipient($sellerDetail['id']);
+
+        return [
+            "sellerCommission" => $sellerCommission,
+            "pagarmeId" => $recipient['pagarme_id']
+        ];
+    }
+
+    private function getTotalSellerCommission(array $splitData)
+    {
+        $totalCommission = 0;
+
+        foreach ($splitData as $commission) {
+            $totalCommission += $commission['sellerCommission'];
+        }
+
+        return $totalCommission;
+    }
+
+    private function sumSellersCommissionsIfArrayKeyExists(
+        int $itemPrice,
+        array $splitData,
+        array $sellerDetail
+    )
+    {
+        $sellerId = $sellerDetail['id'];
+
+        if (array_key_exists($sellerId, $splitData['sellers'])) {
+            $commissionToAdd = $splitData['sellers'][$sellerId]['sellerCommission'];
+            $splitData['sellers'][$sellerId] = $this->getSellerData(
+                $itemPrice,
+                $sellerDetail
+            );
+            $splitData['sellers'][$sellerId]['sellerCommission']
+                = $splitData['sellers'][$sellerId]['sellerCommission'] + $commissionToAdd;
+        }
+
+        return $splitData;
+    }
+
+    public function setNewKeyIfArrayKeyNotExists(
+        int $itemPrice,
+        array $splitData,
+        array $sellerDetail
+    )
+    {
+        $sellerId = $sellerDetail['id'];
+
+        if (!array_key_exists($sellerId, $splitData['sellers'])) {
+            $splitData['sellers'][$sellerId] = $this->getSellerData(
+                $itemPrice,
+                $sellerDetail
+            );
+        }
+
+        return $splitData;
     }
 }
