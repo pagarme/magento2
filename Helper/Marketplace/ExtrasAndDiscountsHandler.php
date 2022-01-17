cd2c77243b06cd6929a1872a6d6bcd4505c65402
netorodrigues <jrodrigueson@gmail.com>
2022-01-17 16:58:33 -0300

    🎨 adds Marketplace Handler

---
+++ Helper/Marketplace/ExtrasAndDiscountsHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Pagarme\Pagarme\Helper\Marketplace;
+
+use Pagarme\Pagarme\Helper\Marketplace\MarketplaceHandler;
+
+class ExtrasAndDiscountsHandler extends MarketplaceHandler
+{
+    public function __construct()
+    {
+    }
+}
