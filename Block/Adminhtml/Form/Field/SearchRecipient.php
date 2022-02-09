5a2ea8b01f4ab198b11b688f35689bdbf7dad32d
Daniel Madureira Batista <daniel.madureira.batista@gmail.com>
2022-02-09 10:48:38 -0300

    :art: Improved layout

--- Block/Adminhtml/Form/Field/SearchRecipient.php
+++ Block/Adminhtml/Form/Field/SearchRecipient.php
@@ -10,13 +10,11 @@ class SearchRecipient extends Field
 
     protected function _renderValue(AbstractElement $element): string
     {
-        $fieldId = $element->getHtmlId();
-        $fieldName = $element->getName();
-        $fieldValue = $element->getEscapedValue();
         $html = '<td class="value">';
         $html .= $this->_getElementHtml($element);
-
-        $html .= '<input type="text" class="input-text admin__control-text" name="{{FIELD_NAME}}" id="{{FIELD_ID}}" placeholder="" value="{{FIELD_VALUE}}">';
+        $html .= '<p>Pagar.me rerecipient id that represents your marketplace</p>';
+        $html .= '</td>';
+        $html .= '<td>';
         $html .= sprintf(
             '<button type="button" class="action-basic" api-url="%s" id="pagarme-get-info" data-action="">
                   <span>%s</span>
@@ -24,10 +22,9 @@ class SearchRecipient extends Field
             $this->getSearchUrl(),
             __('Get info')
         );
-        $html .= '<p>Pagar.me rerecipient id that represents your marketplace</p>';
         $html .= '</td>';
 
-        return str_replace(['{{FIELD_ID}}', '{{FIELD_NAME}}', '{{FIELD_VALUE}}'], [$fieldId, $fieldName, $fieldValue], $html);
+        return $html;
     }
 
     protected function getSearchUrl(): string
