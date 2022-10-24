a83b5188c47858a6566f020993e432f157010c0f
Aislan Cedraz <aislan.cedraz@gmail.com>
2022-10-24 15:31:27 -0300

    :bug: check variable before substr

--- Observer/CreditCardDataAssignObserver.php
+++ Observer/CreditCardDataAssignObserver.php
@@ -74,7 +74,9 @@ class CreditCardDataAssignObserver extends AbstractDataAssignObserver
         }else{
             $info->setAdditionalInformation('cc_saved_card', $additionalData->getCcSavedCard());
             $info->setAdditionalInformation('cc_type', $additionalData->getCcType());
-            $info->setAdditionalInformation('cc_last_4', substr($additionalData->getCcLast4(),-4));
+            if ($additionalData->getCcLast4()) {
+                $info->setAdditionalInformation('cc_last_4', substr($additionalData->getCcLast4(),-4));
+            }
             $info->setAdditionalInformation('cc_token_credit_card', $additionalData->getCcTokenCreditCard());
             $info->addData([
                 'cc_type' => $additionalData->getCcType(),
