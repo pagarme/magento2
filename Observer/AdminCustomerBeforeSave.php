557c2f4899db4d4f555252de9d860243c5fa0867
mauriciohaygert <mauricio.haygert@pagar.me>
2022-12-21 01:25:54 -0300

    Save Customer on Pagarme just on update

--- Observer/AdminCustomerBeforeSave.php
+++ Observer/AdminCustomerBeforeSave.php
@@ -44,14 +44,19 @@ class AdminCustomerBeforeSave implements ObserverInterface
         }
 
         $event = $observer->getEvent();
+
         $platformCustomer = new Magento2PlatformCustomerDecorator($event->getCustomer());
+
+        if (empty($platformCustomer->getPagarmeId())) {
+            return $this;
+        }
 
         $customerService = new CustomerService();
         try {
             $customerService->updateCustomerAtPagarme($platformCustomer);
         } catch (\Exception $exception) {
             $log = new LogService('CustomerService');
-            $log->info($exception->getMessage());
+            $log->info("ola" . $exception->getMessage());
 
             if ($exception->getCode() == 404) {
                 $log->info(
