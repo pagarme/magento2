7e08cbb1047a037ace38d8253824db4b0ddec458
Fabiano Mallmann <25328512+fabiano-mallmann@users.noreply.github.com>
2023-01-19 13:12:10 -0300

    add attribute to ignore missing return type in php8+

--- Concrete/Magento2PlatformInvoiceDecorator.php
+++ Concrete/Magento2PlatformInvoiceDecorator.php
@@ -162,7 +162,8 @@ class Magento2PlatformInvoiceDecorator extends AbstractInvoiceDecorator implemen
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize(): mixed
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize()
     {
         return $this->platformInvoice->getData();
     }
