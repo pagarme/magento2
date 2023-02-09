3c07d70b00a0e190c6b316bae8785879b495fcb7
Ramses Almeida <ramses.almeida@pagar.me>
2023-02-09 16:46:19 -0300

    testing the test HuE

--- e2e/tests/happy-path.e2e.test.js
+++ e2e/tests/happy-path.e2e.test.js
@@ -1,13 +1,20 @@
 const { test, expect } = require('@playwright/test')
+const { searchProduct, selectTheProduct, addToCart, proceedCheckout } = require('../helper/helper')
+const { informEmail, informFirstAndLastName } = require('../helper/pagarme_helper')
 
 test.describe('Happy path checkout', () => {
     test.beforeEach(async ({ page}) => {
-        await page.goto('/')
+        await page.goto('/#')
     })
 
 
-    test('Access the page and select the product', async ({page}) => {
-
+    test('Access the page and search the product', async ({page}) => {
+        await searchProduct(page)
+        await selectTheProduct(page)
+        await addToCart(page)
+        await proceedCheckout(page)
+        await informEmail(page)
+        await informFirstAndLastName(page)
     })
 
 })
