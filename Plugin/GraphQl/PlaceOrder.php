ac0bf3a5cb6f0f0201b134e6f68c8d4862c1652f
Rafael Seiffer Melazzo <rafael.melazzo@stone.com.br>
2023-08-17 15:38:43 -0300

    feat: added pix informations to graphql (#227)
    * Added pix informations to graphql
    
    * Remove the unused function parameter "$subject"
    
    * fix: adding subject parameter in plugin
    
    ---------
    
    Co-authored-by: Mateus Picoloto <mateus.picoloto@pagar.me>

---
+++ Plugin/GraphQl/PlaceOrder.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Pagarme\Pagarme\Plugin\GraphQl;
+
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\OrderFactory;
+use Pagarme\Core\Kernel\ValueObjects\Id\OrderId;
+use Pagarme\Pagarme\Concrete\Magento2CoreSetup;
+
+class PlaceOrder
+{
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var \Magento\Sales\Model\OrderFactory
+     */
+    private $orderFactory;
+
+    /**
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param \Magento\Sales\Model\OrderFactory $orderFactory
+     */
+    public function __construct(
+        OrderRepositoryInterface $orderRepository,
+        OrderFactory $orderFactory
+    ) {
+        $this->orderRepository = $orderRepository;
+        $this->orderFactory = $orderFactory;
+    }
+
+    /**
+     * @param \Magento\QuoteGraphQl\Model\Resolver\PlaceOrder $subject
+     * @param mixed $result
+     * @return mixed
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterResolve(
+        \Magento\QuoteGraphQl\Model\Resolver\PlaceOrder $subject,
+        $result
+    ) {
+        $order = $this->orderFactory->create()->loadByIncrementId($result['order']['order_number']);
+        $payment = $order->getPayment();
+
+
+        if (strpos($payment->getMethod(), "pagarme_pix") === false) {
+            return $result;
+        }
+
+        $lastTransId = $payment->getLastTransId();
+        $orderId = substr($lastTransId, 0, 19);
+
+        Magento2CoreSetup::bootstrap();
+        $orderService= new \Pagarme\Core\Payment\Services\OrderService();
+        $result['pagarme_pix'] = $orderService->getPixQrCodeInfoFromOrder(new OrderId($orderId));
+
+        return $result;
+    }
+}
