474cba36024ae66cc3e94e29ab6b83c8ec3d4339
Rafael Seiffer Melazzo <rafael.melazzo@stone.com.br>
2023-08-17 13:16:15 -0300

    Refactor: Improve observers code (#224)
    * Improve observers code
    
    * Removinh unnecessary else
    
    * refactor: new function to remove if
    
    * added function to remove if

--- Observer/CreditCardDataAssignObserver.php
+++ Observer/CreditCardDataAssignObserver.php
@@ -12,17 +12,12 @@
 namespace Pagarme\Pagarme\Observer;
 
 
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\DataObject;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Framework\Event\Observer;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Quote\Api\Data\PaymentInterface;
-use Pagarme\Core\Payment\Repositories\SavedCardRepository;
-use Pagarme\Pagarme\Concrete\Magento2CoreSetup;
-use Pagarme\Pagarme\Model\Cards;
-use Pagarme\Pagarme\Model\CardsRepository;
 use Pagarme\Pagarme\Helper\MultiBuyerDataAssign;
+use Pagarme\Pagarme\Model\CardsRepository;
 
 class CreditCardDataAssignObserver extends AbstractDataAssignObserver
 {
@@ -57,49 +52,84 @@ class CreditCardDataAssignObserver extends AbstractDataAssignObserver
 
         $info->setAdditionalInformation('cc_saved_card', '0');
 
-        if ($additionalData->getCcSavedCard()) {
-            $cardId = $additionalData->getCcSavedCard();
-            $card = $this->cardsRepository->getById($cardId);
+        $this->fillCardData($additionalData, $info);
 
-            $info->setAdditionalInformation('cc_saved_card', $additionalData->getCcSavedCard());
-            $info->setAdditionalInformation('cc_type', $card->getBrand());
-            $info->setAdditionalInformation(
-                'cc_last_4',
-                (string) $card->getLastFourNumbers()
-            );
-            $info->addData([
-                'cc_type' => $card->getBrand(),
-                'cc_owner' => $card->getCardHolderName(),
-                'cc_last_4' => (string) $card->getLastFourNumbers()
-            ]);
-        }else{
-            $info->setAdditionalInformation('cc_saved_card', $additionalData->getCcSavedCard());
-            $info->setAdditionalInformation('cc_type', $additionalData->getCcType());
-            if ($additionalData->getCcLast4()) {
-                $info->setAdditionalInformation('cc_last_4', substr($additionalData->getCcLast4(),-4));
-            }
-            $info->setAdditionalInformation('cc_token_credit_card', $additionalData->getCcTokenCreditCard());
-            $info->addData([
-                'cc_type' => $additionalData->getCcType(),
-                'cc_owner' => $additionalData->getCcOwner(),
-                'cc_last_4' => $additionalData->getCcLast4(),
-                'cc_exp_month' => $additionalData->getCcExpMonth(),
-                'cc_exp_year' => $additionalData->getCcExpYear(),
-                'cc_token_credit_card' => $additionalData->getCcTokenCreditCard(),
-            ]);
-
-            $info->setAdditionalInformation('cc_savecard', $additionalData->getCcSavecard());
-        }
         $multiBuyerDataAssign = new MultiBuyerDataAssign();
         $multiBuyerDataAssign->setCcMultiBuyer($info, $additionalData);
         
         $info->setAdditionalInformation('cc_installments', 1);
 
         if ($additionalData->getCcInstallments()) {
-            $info->setAdditionalInformation('cc_installments', (int) $additionalData->getCcInstallments());
+            $info->setAdditionalInformation(
+                'cc_installments',
+                (int) $additionalData->getCcInstallments()
+            );
         }
 
         return $this;
+    }
+
+    /**
+     * @param \Magento\Framework\DataObject $additionalData
+     * @param mixed $info
+     * @return void
+     */
+    public function fillCardData(DataObject $additionalData, $info)
+    {
+        if ($additionalData->getCcSavedCard()) {
+              $this->fillSavedCardData($additionalData, $info);
+              return;
+        }
+        $this->fillNotSavedCardData($info, $additionalData);
+    }
+
+    /**
+     * @param DataObject $additionalData
+     * @param $info
+     */
+    private function fillSavedCardData(DataObject $additionalData, $info)
+    {
+        $cardId = $additionalData->getCcSavedCard();
+        $card = $this->cardsRepository->getById($cardId);
+
+        $info->setAdditionalInformation('cc_saved_card', $additionalData->getCcSavedCard());
+        $info->setAdditionalInformation('cc_type', $card->getBrand());
+        $info->setAdditionalInformation(
+            'cc_last_4',
+            (string) $card->getLastFourNumbers()
+        );
+        $info->addData([
+            'cc_type' => $card->getBrand(),
+            'cc_owner' => $card->getCardHolderName(),
+            'cc_last_4' => (string) $card->getLastFourNumbers()
+        ]);
+    }
+
+    /**
+     * @param $info
+     * @param DataObject $additionalData
+     */
+    private function fillNotSavedCardData($info, DataObject $additionalData)
+    {
+        $info->setAdditionalInformation('cc_saved_card', $additionalData->getCcSavedCard());
+        $info->setAdditionalInformation('cc_type', $additionalData->getCcType());
+        if ($additionalData->getCcLast4()) {
+            $info->setAdditionalInformation(
+                'cc_last_4',
+                substr($additionalData->getCcLast4(),-4)
+            );
+        }
+        $info->setAdditionalInformation('cc_token_credit_card', $additionalData->getCcTokenCreditCard());
+        $info->addData([
+            'cc_type' => $additionalData->getCcType(),
+            'cc_owner' => $additionalData->getCcOwner(),
+            'cc_last_4' => $additionalData->getCcLast4(),
+            'cc_exp_month' => $additionalData->getCcExpMonth(),
+            'cc_exp_year' => $additionalData->getCcExpYear(),
+            'cc_token_credit_card' => $additionalData->getCcTokenCreditCard(),
+        ]);
+
+        $info->setAdditionalInformation('cc_savecard', $additionalData->getCcSavecard());
     }
 
 }
