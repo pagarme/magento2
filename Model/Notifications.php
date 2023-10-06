80be2f8ac7def3acf2996bf12e64f0ef5e77a2c5
RafaMelazzo <rafael.melazzo@stone.com.br>
2023-10-06 10:19:10 -0300

    improving notifications

--- Model/Notifications.php
+++ Model/Notifications.php
@@ -61,12 +61,16 @@ class Notifications extends Message
      */
     public function getText(): ?string
     {
-        $html = null;
-        $count = count($this->warnings);
-
-        for ($i = 0; $i < $count; $i++) {
-            $html .= '<div style="padding-bottom: 10px;' . (($i !== 0) ? 'margin-top: 10px;' : '') . (($i < $count - 1) ? 'border-bottom: 1px dashed #d1d1d1' : '') . '">' . (($i === 0) ? "<b style='display:block; margin-bottom:8px'> Pagar.me </b>" : '') . "<span style='background-color:red; color:white; padding:2px 5px'>" . __('Important!') . "</span> " . $this->warnings[$i] . '</div>';
+        if (empty($this->warnings)) {
+            return null;
         }
+
+        $html = '<div class="pagarme-admin-warnings"><h3>Pagar.me</h3>';
+        foreach ($this->warnings as $warning) {
+            $html .= "<div>{$warning}</div>";
+        }
+        $html .= '</div>';
+
         return $html;
     }
 
@@ -91,11 +95,16 @@ class Notifications extends Message
     private function addEnvorimentMessages()
     {
         if (!$this->config->isHubEnabled()) {
-            $this->warnings[] = __('Pagar.me module is not yet integrated to the HUB. Complete the integration to start selling!');
+            $this->warnings[] = __(
+                'Pagar.me module is not yet integrated to the HUB. Complete the integration to start selling!'
+            );
         }
 
         if ($this->config->isSandboxMode()) {
-            $this->warnings[] = __('This store is linked to the Pagar.me test environment. This environment is intended for integration validation and does not generate real financial transactions.');
+            $this->warnings[] = __(
+                'This store is linked to the Pagar.me test environment. This environment is intended for integration'
+                . ' validation and does not generate real financial transactions.'
+            );
         }
     }
 
@@ -104,11 +113,24 @@ class Notifications extends Message
         $customerConfigs = $this->config->getPagarmeCustomerConfigs();
 
         if ($customerConfigs['showVatNumber'] != 1) {
-            $this->warnings[] = __("<b>Show VAT Number on Storefront</b> must be defined as <b>'Yes'</b> on <b>Stores</b> > <b>Configuration</b> > <b>Customer</b> > <b>Customer Configuration</b> > <b>Create New Account Options</b> for Pagar.me module to work on your store.");
+            $this->warnings[] = __(
+                "<b>Show VAT Number on Storefront</b> must be defined as <b>'Yes'</b> on "
+                . "<b>Stores</b> > <b>Configuration</b> > <b>Customer</b> > <b>Customer Configuration</b> > <b>Create "
+                . "New Account Options</b> for Pagar.me module to work on your store."
+            );
         }
 
         if ($customerConfigs['streetLinesNumber'] != 4) {
-            $this->warnings[] = __("<b>Number of Lines in a Street Address</b> must be defined as <b>'4'</b> on <b>Stores</b> > <b>Configuration</b> > <b>Customer</b> > <b>Customer Configuration</b> > <b>Name and Address Options</b> for Pagar.me module to work on your store.");
+            $this->warnings[] = __(
+                "<b>Number of Lines in a Street Address</b> must be defined as <b>'4'</b> on <b>Stores</b> > "
+                . "<b>Configuration</b> > <b>Customer</b> > <b>Customer Configuration</b> > <b>Name and Address "
+                . "Options</b> for Pagar.me module to work on your store."
+            );
         }
+    }
+
+    private function addHubDomainMessages()
+    {
+//        $hubConfigs = $this->config->
     }
 }
