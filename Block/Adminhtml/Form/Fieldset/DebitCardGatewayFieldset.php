5c84da7afa7dab31461268f55fb4a9ab6a6521a0
Fabiano Mallmann <25328512+fabiano-mallmann@users.noreply.github.com>
2023-11-21 12:15:15 -0300

    add translates and add gateway validation to debit card

---
+++ Block/Adminhtml/Form/Fieldset/DebitCardGatewayFieldset.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Pagarme\Pagarme\Block\Adminhtml\Form\Fieldset;
+
+use Magento\Backend\Block\Context;
+use Magento\Backend\Model\Auth\Session;
+use Magento\Config\Block\System\Config\Form\Fieldset;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\View\Helper\Js;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+use Pagarme\Pagarme\Model\Account;
+use Pagarme\Pagarme\Model\PagarmeConfigProvider;
+
+class DebitCardGatewayFieldset extends Fieldset
+{
+    /**
+     * @var Account
+     */
+    protected $account;
+
+    /**
+     * @param Context $context
+     * @param Session $authSession
+     * @param Js $jsHelper
+     * @param Account $account
+     * @param array $data
+     * @param SecureHtmlRenderer|null $secureRenderer
+     */
+    public function __construct(
+        Context $context,
+        Session $authSession,
+        Js $jsHelper,
+        Account $account,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    ) {
+        parent::__construct($context, $authSession, $jsHelper, $data, $secureRenderer);
+        $this->account = $account;
+    }
+
+    /**
+     * @param AbstractElement $element
+     * @return string
+     */
+    public function render(AbstractElement $element)
+    {
+        if (!$this->account->isGateway(PagarmeConfigProvider::DEBIT_PAYMENT_CONFIG)) {
+            return '';
+        }
+
+        return parent::render($element);
+    }
+}
--- Gateway/Transaction/Base/Config/TdsConfigInterface.php
+++ Gateway/Transaction/Base/Config/TdsConfigInterface.php
@@ -5,7 +5,6 @@ namespace Pagarme\Pagarme\Gateway\Transaction\Base\Config;
 interface TdsConfigInterface
 {
 
-    // const TDS = static::TDS;
     public function getTdsActive();
     public function getOrderWithTdsRefused();
 }
