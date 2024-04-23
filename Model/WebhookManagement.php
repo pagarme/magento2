1bcda4c76fda3e467a98a37cc027965489186913
Mateus Picoloto <mateus.picoloto@pagar.me>
2024-04-23 09:22:47 -0300

    feat: added recipient webhook validation

--- Model/WebhookManagement.php
+++ Model/WebhookManagement.php
@@ -52,7 +52,10 @@ class WebhookManagement implements WebhookManagementInterface
             $postData->type = $type;
             $postData->data = $data;
 
-            if($this->hasMagentoOrder($data) === false) {
+            if (
+                $this->hasMagentoOrder($data) === false
+                && $this->isNotRecipientWebhook($type)
+            ) {
                 $this->logWebhookIdCaseExistsMetadata($data, $id);
                 return [
                     "message" => "Webhook Received",
@@ -71,7 +74,7 @@ class WebhookManagement implements WebhookManagementInterface
                 "message" => $e->getMessage(),
                 "code" => 200
             ];
-        } catch(AbstractPagarmeCoreException $e) {
+        } catch (AbstractPagarmeCoreException $e) {
             throw new M2WebApiException(
                 new Phrase($e->getMessage()),
                 0,
@@ -82,41 +85,49 @@ class WebhookManagement implements WebhookManagementInterface
     private function logWebhookIdCaseExistsMetadata($webhookData, $webhookId)
     {
         $metadata = $this->getMetadata($webhookData);
-        if($metadata === false || !array_key_exists('platformVersion', $metadata)) {
+        if ($metadata === false || !array_key_exists('platformVersion', $metadata)) {
             return;
         }
-        if(strpos($metadata['platformVersion'], "Magento") !== false) {
+        if (strpos($metadata['platformVersion'], "Magento") !== false) {
             $logService = new LogService(
                 'Webhook',
                 true
             );
             $logService->info(
                 "Webhook Received but not proccessed",
-                (object)['webhookId' => $webhookId
-            ]);
+                (object)[
+                    'webhookId' => $webhookId
+                ]
+            );
         }
     }
     private function getMetadata($data)
     {
         $metadata = false;
-        if(!array_key_exists('order', $data) && !array_key_exists('subscription', $data)) {
+        if (!array_key_exists('order', $data) && !array_key_exists('subscription', $data)) {
             return false;
         }
-        if(array_key_exists('metadata', $data)) {
+        if (array_key_exists('metadata', $data)) {
             $metadata = $data['metadata'];
         }
         return $metadata;
     }
+
     private function hasMagentoOrder($data)
     {
         $code = 0;
-        if(array_key_exists('subscription', $data)) {
+        if (array_key_exists('subscription', $data)) {
             $code = $data['subscription']['code'];
         }
-        if(array_key_exists('order', $data)) {
+        if (array_key_exists('order', $data)) {
             $code = $data['order']['code'];
         }
         $order = $this->orderFactory->create()->loadByIncrementId($code);
         return $order->getId() ?? false;
+    }
+
+    private function isNotRecipientWebhook($type)
+    {
+        return strpos($type, 'recipient') === false;
     }
 }
